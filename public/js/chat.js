f456488b21de5f3ecaf1af01cb8ac6957176f1da
Akshay saxena <51820212+akshay-saxena-it@users.noreply.github.com>
2021-05-15 18:01:07 +0530

    one-to-one

---
+++ public/js/chat.js
@@ -0,0 +1,93 @@
+const login = () => {
+    let username = $('#login_name').val();
+    let password = $('#login_pass').val();
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8082/login",
+        data: JSON.stringify({ "username": username, "password": password }),
+        success: function (resp) {
+            if (resp.status) {
+                $('#login').hide();
+                $('#after-login').show();
+                sessionStorage.setItem("user", JSON.stringify(resp.data));
+                $('#me').html(`
+                        <div class="me">
+                            <img src="images/${resp.data.user_image}" />
+                            ${resp.data.user_full_name}
+                         </div>
+                         `);
+                socket.emit('loggedin', resp.data);
+            }
+        },
+        error: function (err) {
+            console.log('error', err)
+        },
+        dataType: "json",
+        contentType: "application/json"
+    });
+}
+
+const sendMyMessage = (chatWidowId, fromUser, message) => {
+    let loggedInUser = JSON.parse(sessionStorage.getItem('user'))
+    let meClass = loggedInUser.user_id == fromUser.user_id ? 'me' : '';
+    let image = loggedInUser.user_id == fromUser.user_id ? fromUser.user_image : 'another_user.jpg';
+
+    $('#after-login').find(`#${chatWidowId} .body`).append(`
+        <div class="chat-text ${meClass}">
+            <div class="userPhoto">
+                <img src="images/${image}" />
+            </div>
+            <div>
+                <span class="message">${message}<span>
+            </div>
+        </div>
+    `);
+}
+
+const sendMessage = (room) => {
+    let loggedInUser = JSON.parse(sessionStorage.getItem('user'))
+    let message = $('#' + room).find('.messageText').val();
+    $('#' + room).find('.messageText').val('');
+    socket.emit('message', { room: room, message: message, from: loggedInUser });
+    sendMyMessage(room, loggedInUser, message)
+}
+const openChatWindow = (room) => {
+    if ($(`#${room}`).length === 0) {
+        $('#after-login').append(`
+        <div class="chat-window" id="${room}">
+            <div class="body"></div>
+            <div class="footer">
+                <input type="text" class="messageText"/><button onclick="sendMessage('${room}')">Send</button>
+            </div>
+        </div>
+        `)
+    }
+}
+const createRoom = (id) => {
+    let loggedInUser = JSON.parse(sessionStorage.getItem('user'));
+    let room = Date.now() + Math.random();
+    room = room.toString().replace(".", "_");
+    let data = { room: room, userId: loggedInUser.userId, withUserId: id }
+    socket.emit('create', data);
+    openChatWindow(room);
+}
+socket.on('updateUserList', function (userList) {
+    let loggedInUser = JSON.parse(sessionStorage.getItem('user'));
+    $('#user-list').html('<h4>ONLINE USERS</h4><ul></ul>');
+    userList.forEach(item => {
+        if (loggedInUser.user_id != item.user_id) {
+            $('#user-list ul').append(`<li data-id="${item.user_id}" onclick="createRoom('${item.user_id}')">${item.user_full_name}</li>`)
+        }
+    });
+});
+
+socket.on('invite', function (data) {
+    socket.emit("joinRoom", data)
+});
+socket.on('message', function (msg) {
+    //If chat window not opened with this roomId, open it
+    if (!$('#after-login').find(`#${msg.room}`).length) {
+        openChatWindow(msg.room)
+    }
+    sendMyMessage(msg.room, msg.from, msg.message)
+});
